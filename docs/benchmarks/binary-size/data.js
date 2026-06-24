cdd341251e8755a279daabd5e09c915caaf2569f
github-action-benchmark <github@users.noreply.github.com>
2026-06-24 00:58:03 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 02d7b464d16f7fe6afed42acc859fbd72dd473d6

--- docs/benchmarks/binary-size/data.js
+++ docs/benchmarks/binary-size/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1782176494006,
+  "lastUpdate": 1782262683078,
   "repoUrl": "https://github.com/lquerel/otel-arrow",
   "entries": {
     "Benchmark": [
@@ -26,6 +26,38 @@ window.BENCHMARK_DATA = {
           {
             "name": "linux-amd64-binary-size",
             "value": 112.18,
+            "unit": "MB"
+          },
+          {
+            "name": "linux-arm64-binary-size",
+            "value": 99.53,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "albertlockett",
+            "username": "albertlockett",
+            "email": "a.lockett@f5.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "02d7b464d16f7fe6afed42acc859fbd72dd473d6",
+          "message": "chore: upgrade kube rust crate versions (#3334)\n\n# Change Summary\n\n<!--\nReplace with a brief summary of the change in this PR\n-->\n\nupgrade k8s-openapi to 0.28.0\nupgrade kube to 4.0.0\n\nhopefully fix compile issue w/\nhttps://github.com/open-telemetry/otel-arrow/pull/3330\n\n## What issue does this PR close?\n\n<!--\nWe highly recommend correlation of every PR to an issue\n-->\n\n* Closes #NNN\n\n## How are these changes tested?\n\n## Are there any user-facing changes?\n\n <!-- If yes, provide further info below -->\n\n### Changelog\n\n<!--\nUser-facing changes need a .chloggen/*.yaml entry. Copy the\nTEMPLATE.yaml\nin go/.chloggen/ or rust/otap-dataflow/.chloggen/ and fill in the\nfields.\nIf not required, include `chore` in the PR title.\n-->\n\n* [ ] Added a `.chloggen/*.yaml` entry, OR this PR is a `chore`\n(indicated in title).",
+          "timestamp": "2026-06-22T13:31:01Z",
+          "url": "https://github.com/lquerel/otel-arrow/commit/02d7b464d16f7fe6afed42acc859fbd72dd473d6"
+        },
+        "date": 1782262677689,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "linux-amd64-binary-size",
+            "value": 112.17,
             "unit": "MB"
           },
           {
